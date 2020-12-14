bd330ff5cf1bfbbcd7948109aa94e29e2d0f0714
butterbroad <pyatochevskiy@gmail.com>
2020-12-14 18:54:49 +0300

    initial commit

---
+++ comeback/js/script.js
@@ -0,0 +1,105 @@
+'use strict'
+
+//links with params
+const getLinkWithParams = document.querySelectorAll('.get-link');
+
+if (getLinkWithParams.length > 0) {
+  getLinkWithParams.forEach(item => {
+    item.setAttribute('href', item.getAttribute('href') + window.location.search);
+  });
+}
+
+//mobile links
+const isMobile = () => {
+  return (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  );
+};
+
+const hasRegistrationLink = isMobile()
+  ? 'https://m.maxline.by/register'
+  : 'https://maxline.by/user/signup';
+
+
+const regLinks = document.querySelectorAll('.reg-link');
+regLinks.forEach(link => {
+  link.setAttribute('href', hasRegistrationLink);
+});
+
+const mobLinks = document.querySelectorAll('.mob-link');
+mobLinks.forEach(link => {
+  let linkHref = link.getAttribute('href');
+  let linkArray = linkHref.split("https://")
+  let newLink = isMobile() ? `https://m.${linkArray[1]}` : linkHref;
+  link.setAttribute('href', newLink);
+});
+
+//modal 
+// const modalTrigger = document.querySelector('.hero__info');
+// const modal = document.querySelector('.modal');
+
+// function toggleModal() {
+//   modal.classList.toggle('active');
+
+//   const modalClose = document.querySelector('.modal-info__close');
+//   modalClose.addEventListener('click', () => {
+//     modal.classList.remove('active');
+//   });
+// }
+
+// modalTrigger.addEventListener('click', (e) => {
+//   e.stopPropagation();
+//   toggleModal();
+// });
+
+// document.addEventListener('click', e => {
+//   let target = e.target;
+//   let isModal = target == modal;
+//   let isTrigger = target == modalTrigger;
+//   let menuIsActive = modal.classList.contains('active');
+//   if (isModal && !isTrigger && menuIsActive) {
+//     toggleModal();
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
